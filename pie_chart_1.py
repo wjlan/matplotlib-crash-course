10b3eee9e1806b7edc54de66d36e28e0795236ba
wanjun <wanjunlan@gmail.com>
2024-01-10 15:17:39 -0800

    add pie chart 1

--- pie_chart_1.py
+++ pie_chart_1.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+fifa = pd.read_csv('fifa_data.csv')
+
 left = fifa.loc[fifa['Preferred Foot'] == 'Left'].count()[0]
 right = fifa.loc[fifa['Preferred Foot'] == 'Right'].count()[0]
 
